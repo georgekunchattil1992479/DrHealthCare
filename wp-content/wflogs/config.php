954757556099d6612a9bae68bfd8f44d7d88a751
George Jacob <georgekunchattil@gmail.com>
2021-04-13 23:45:33 +0530

    Added some essential plugins

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1618941734;s:7:"authKey";s:64:"*I?VjwkK*,gZl#$p^{b?]s9h33_@Iya{2Tvx^((]99.zL]8i`Z0Iu0~xpYTJzV!Z";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2217;}
